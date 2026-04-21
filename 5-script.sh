8a71329cdeeb256741b4a86839d424759b2a7de3
vansh-09 <vanshkjain09@gmail.com>
2026-04-21 23:24:16 +0530

    5th rev

--- 5-script.sh
+++ 5-script.sh
@@ -1,26 +1,17 @@
-#!/bin/bash
-
 echo "Enter n:"
 read n
 
 a=0
 b=1
 
-echo "Fibonacci:"
-for ((i=0;i<n;i++))
-do
-    echo -n "$a "
-    temp=$((a+b))
-    a=$b
-    b=$temp
-done
+echo "Fibonacci Series:"
+echo $a
+echo $b
 
-echo -e "\nPattern:"
-for ((i=1;i<=n;i++))
+for ((i=2; i<$n; i++))
 do
-    for ((j=1;j<=i;j++))
-    do
-        echo -n "$j"
-    done
-    echo
+    c=$((a+b))
+    echo $c
+    a=$b
+    b=$c
 done
